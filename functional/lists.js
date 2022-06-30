659dae440670b2e9f3227e6a544e88c1dedbdfe6
fmfontana <105196681+fmfontana@users.noreply.github.com>
2022-06-30 10:22:34 -0300

    .

--- functional/lists.js
+++ functional/lists.js
@@ -38,6 +38,16 @@ function addnLoop(...fns) {
 	return addFn(fns[0], fns[1]);
 }
 
+function addnRecursive(fn0, fn1, ...rest) {
+	if (rest.length == 0) {
+		return addFn(fn0, fn1);
+	}
+
+	return addnRecursive(function f() {
+		return addFn(fn0, fn1);
+	}, ...rest);
+}
+
 console.log(fn1() === 10);
 console.log(fn2() === 20);
 console.log(add(fn1(), fn2()) === 30);
@@ -45,3 +55,4 @@ console.log(addFn(fn1, fn2) === 30);
 console.log(retFn(fn1()) === 10);
 console.log(returnValFn(fn1())() === 10);
 console.log(addnLoop(fn1, fn2, returnValFn(4), returnValFn(5)) === 39);
+console.log(addnRecursive(returnValFn(10), returnValFn(80), returnValFn(4), returnValFn(5)) === 99);
